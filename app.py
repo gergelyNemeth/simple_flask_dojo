59859cc230b19249c901e4f9b9af42477a4e4797
krisztian <alt.krisztian@gmail.com>
2017-05-11 09:59:18 +0200

    Implement request counter

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
+counts = 0
 
 
 @app.route('/')
 @app.route('/request-counter', methods=["GET", "POST"])
 def counter():
+    global counts
+    if request.url.endswith("/request-counter"):
+        counts += 1
+        print(counts)
+        return redirect('/')
     return render_template("counter.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)

519c23f9afd5f2371ea249f93ca00c18964c255f
krisztian <alt.krisztian@gmail.com>
2017-05-11 09:36:49 +0200

    Implement render_template

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+@app.route('/')
 @app.route('/request-counter')
 def counter():
-    return "Hello world"
+    return render_template("counter.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
